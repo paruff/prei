21f02cdce51845a7a98e10a9b02cc3516723a6de
Phil Ruff <phil.ruff@gmail.com>
2026-07-10 10:27:18 +0100

    fix: remove unused imports in growth.py

--- core/models/growth.py
+++ core/models/growth.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 from decimal import Decimal
-from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MinValueValidator
 from django.db import models
 
 User = get_user_model()
